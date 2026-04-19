a286d694cf58adb5a0d517df0add4a8fd3ae90c8
Pedro <pe25171@mit.edu>
2026-04-19 12:30:04 -0700

    feat: add MapCameraController to wire RenderTexture to MapDisplay RawImage

---
+++ Assets/Scripts/UI/MapCameraController.cs
@@ -0,0 +1,50 @@
+// Assets/Scripts/UI/MapCameraController.cs
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DnD.UI
+{
+    [RequireComponent(typeof(Camera))]
+    public class MapCameraController : MonoBehaviour
+    {
+        public static MapCameraController Instance { get; private set; }
+
+        [SerializeField] private int renderWidth  = 1024;
+        [SerializeField] private int renderHeight = 1024;
+
+        private RenderTexture renderTexture;
+        private Camera mapCamera;
+
+        private void Awake()
+        {
+            Instance = this;
+            mapCamera = GetComponent<Camera>();
+
+            renderTexture = new RenderTexture(renderWidth, renderHeight, 16, RenderTextureFormat.ARGB32);
+            renderTexture.Create();
+            mapCamera.targetTexture = renderTexture;
+
+            // Find the RawImage in the scene named "MapDisplay" and assign texture
+            var rawImages = FindObjectsOfType<RawImage>();
+            foreach (var ri in rawImages)
+            {
+                if (ri.gameObject.name == "MapDisplay")
+                {
+                    ri.texture = renderTexture;
+                    break;
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (renderTexture != null)
+            {
+                renderTexture.Release();
+                Destroy(renderTexture);
+            }
+        }
+
+        public Camera MapCamera => mapCamera;
+    }
+}
